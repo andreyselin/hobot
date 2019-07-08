96ac0a201848db77397e0ee00492b7a1978bd5b1
andreyselin <andreiprostoandrei@gmail.com>
2019-07-08 23:20:27 +0200

    Chore code and add comments to index

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,20 @@ import { IContext } from "./BotModel";
 
 
 // Paste your token to try basic bot functionality
-const token = '_____USE YOUR TOKEN HERE_____';
+const token = '864069020:AAElEX_6ZHLzIo0BfKNjVx_7SJvzfzocDdc';
 
+// Running bot wrapper
 const botServer = new BotServer(token);
+
+
+// Create your first route pointing at startContainer
 botServer.createRoute(startController);
 
-// Tell bot to work with controller under '/start'
-// route when /start command is passed
+// After setting up your first container you probably want to use it
+// when user takes first interaction with it (start command).
+
+// Tell bot to work with controller under '/start' route when /start command is received
 botServer.bot.start((ctx: IContext) => botServer.gotoPath(ctx, '/start'));
+
+// Yщu can create other containers and navigate to them by their path
+// with botServer.gotoPath(ctx, pathString);
